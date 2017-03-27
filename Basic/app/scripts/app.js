42cca9a95fe69d8a19e832fc67b9366202b2ce71
Yulishan <yulishan3@gmail.com>
2017-03-27 13:58:00 +0800

    initial commit

---
+++ Basic/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name basicApp
+ * @description
+ * # basicApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('basicApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
